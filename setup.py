f3a14b248fd8c4d155debae76d86b4576626b818
Florian-BARRE <flo.others@gmail.com>
2025-02-13 17:08:56 +0100

    V0.1.4: fix multiprocess sync error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="taskbrain",
-    version="0.1.3",
+    version="0.1.4",
     author="Florian BARRE",
     author_email="florian.barre78@gmail.com",
     description="A simple and powerful task scheduler.",
